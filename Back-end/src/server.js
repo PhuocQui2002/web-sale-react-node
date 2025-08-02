d3cf96eb5804ae694525efb1b3eeb7182b8a2e2c
Qui <phuocqui107870@gmail.com>
2025-08-02 21:31:08 +0700

    update

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -10,7 +10,7 @@ const app = express();
 require("dotenv").config();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json());
 const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
